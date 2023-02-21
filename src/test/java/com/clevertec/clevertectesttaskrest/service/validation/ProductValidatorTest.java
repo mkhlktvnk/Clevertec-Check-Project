60a0f476665e6f86b07a7a21879042c954737f1c
misha <mikhail.kutovenko@gmail.com>
2023-02-21 18:30:49 +0300

    add ProductValidatorTest

---
+++ src/test/java/com/clevertec/clevertectesttaskrest/service/validation/ProductValidatorTest.java
@@ -0,0 +1,35 @@
+package com.clevertec.clevertectesttaskrest.service.validation;
+
+import com.clevertec.clevertectesttaskrest.builder.impl.ProductTestBuilder;
+import com.clevertec.clevertectesttaskrest.domain.Product;
+import com.clevertec.clevertectesttaskrest.service.exception.IncorrectRequestException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ProductValidatorTest {
+    @Test
+    void checkValidateProductShouldThrowIncorrectRequestExceptionWhenNameIsInvalid() {
+        Product product = ProductTestBuilder.aProduct()
+                .name("")
+                .price(BigDecimal.valueOf(99.99))
+                .build();
+
+        assertThrows(IncorrectRequestException.class, () -> ProductValidator.validateProduct(product));
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {0.00, 0.000, 999.9999, 12345.12345, 99999999999999999999999.99, -1.0, -324})
+    void checkValidateProductShouldThrowIncorrectRequestExceptionWhenPriceIsInvalid(double price) {
+        Product product = ProductTestBuilder.aProduct()
+                .name("")
+                .price(BigDecimal.valueOf(price))
+                .build();
+
+        assertThrows(IncorrectRequestException.class, () -> ProductValidator.validateProduct(product));
+    }
+}
