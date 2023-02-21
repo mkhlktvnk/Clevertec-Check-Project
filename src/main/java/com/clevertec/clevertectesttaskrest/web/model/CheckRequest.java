5b9c6054970167ae57e4fb372b8610d0c6660d09
misha <mikhail.kutovenko@gmail.com>
2023-02-21 16:49:44 +0300

    add @EqualsAndHashCode

--- src/main/java/com/clevertec/clevertectesttaskrest/web/model/CheckRequest.java
+++ src/main/java/com/clevertec/clevertectesttaskrest/web/model/CheckRequest.java
@@ -1,5 +1,6 @@
 package com.clevertec.clevertectesttaskrest.web.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 public class CheckRequest {
     private List<CheckRequestPosition> items;
     private Long discountCardId;
