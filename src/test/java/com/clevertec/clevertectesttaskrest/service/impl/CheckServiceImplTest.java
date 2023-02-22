1f5bae97bd597ce795d5e3e143b2996897f333f2
misha <mikhail.kutovenko@gmail.com>
2023-02-22 10:50:52 +0300

    add test for createCheck method

--- src/test/java/com/clevertec/clevertectesttaskrest/service/impl/CheckServiceImplTest.java
+++ src/test/java/com/clevertec/clevertectesttaskrest/service/impl/CheckServiceImplTest.java
@@ -1,14 +1,22 @@
 package com.clevertec.clevertectesttaskrest.service.impl;
 
+import com.clevertec.clevertectesttaskrest.builder.impl.CheckRequestTestBuilder;
 import com.clevertec.clevertectesttaskrest.builder.impl.CheckTestBuilder;
+import com.clevertec.clevertectesttaskrest.builder.impl.ProductTestBuilder;
 import com.clevertec.clevertectesttaskrest.domain.Check;
+import com.clevertec.clevertectesttaskrest.domain.DiscountCard;
+import com.clevertec.clevertectesttaskrest.domain.Product;
 import com.clevertec.clevertectesttaskrest.repository.CheckRepository;
 import com.clevertec.clevertectesttaskrest.service.CheckItemService;
 import com.clevertec.clevertectesttaskrest.service.DiscountCardService;
 import com.clevertec.clevertectesttaskrest.service.ProductService;
 import com.clevertec.clevertectesttaskrest.service.exception.EntityNotFoundException;
+import com.clevertec.clevertectesttaskrest.web.model.CheckRequest;
+import com.clevertec.clevertectesttaskrest.web.model.CheckRequestPosition;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -17,11 +25,15 @@ import org.springframework.data.domain.Pageable;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 import static com.clevertec.clevertectesttaskrest.builder.impl.CheckTestBuilder.aCheck;
+import static com.clevertec.clevertectesttaskrest.builder.impl.DiscountCardTestBuilder.aDiscountCard;
+import static com.clevertec.clevertectesttaskrest.builder.impl.ProductTestBuilder.aProduct;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -29,6 +41,15 @@ import static org.mockito.Mockito.when;
 class CheckServiceImplTest {
     @Mock
     private CheckRepository checkRepository;
+
+    @Mock
+    private ProductServiceImpl productService;
+
+    @Mock
+    private DiscountCardServiceImpl discountCardService;
+
+    @Mock
+    private CheckItemServiceImpl checkItemService;
 
     @InjectMocks
     private CheckServiceImpl checkService;
@@ -70,5 +91,32 @@ class CheckServiceImplTest {
         when(checkRepository.findById(ID)).thenReturn(Optional.empty());
 
         assertThrows(EntityNotFoundException.class, () -> checkService.getById(ID));
+    }
+
+    @Test
+    void createCheckShouldCallServices() {
+        CheckRequest checkRequest = CheckRequestTestBuilder.aCheckRequest()
+                .discountCardId(1L)
+                .positions(List.of(
+                        new CheckRequestPosition(1L, 1),
+                        new CheckRequestPosition(2L, 1),
+                        new CheckRequestPosition(3L, 1)
+                ))
+                .build();
+        List<Product> products = List.of(
+                aProduct().id(1L).name("name-1").price(BigDecimal.valueOf(10.00)).build(),
+                aProduct().id(1L).name("name-2").price(BigDecimal.valueOf(20.00)).build(),
+                aProduct().id(1L).name("name-3").price(BigDecimal.valueOf(30.00)).build()
+        );
+        DiscountCard discountCard = aDiscountCard().id(1L).discount(20).build();
+        when(productService.getProductsByIdIn(any())).thenReturn(products);
+        when(discountCardService.getById(checkRequest.getDiscountCardId())).thenReturn(discountCard);
+
+        checkService.createCheck(checkRequest);
+
+        verify(productService).getProductsByIdIn(anySet());
+        verify(checkItemService).saveAllItems(anySet());
+        verify(discountCardService).getById(checkRequest.getDiscountCardId());
+        verify(checkRepository).save(any(Check.class));
     }
 }
