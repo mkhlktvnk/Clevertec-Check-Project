2cedd95f0cb82d59a278f7bceaeddc7e97150d92
misha <mikhail.kutovenko@gmail.com>
2023-02-23 11:59:42 +0300

    Add double quote constant

--- src/main/java/com/clevertec/clevertectesttaskrest/jsonlib/constant/JsonSigns.java
+++ src/main/java/com/clevertec/clevertectesttaskrest/jsonlib/constant/JsonSigns.java
@@ -7,5 +7,6 @@ public class JsonSigns {
     public static final String CLOSING_BRACKET = "]";
     public static final String COMMA = ",";
     public static final String COLON = ":";
+    public static final String DOUBLE_QUOTE = "\"%s\"";
     public static final String NULL = "null";
 }
