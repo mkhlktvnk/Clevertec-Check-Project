91f558a34a34bf82e939cfbe71ed965ece81b0e4
misha <mikhail.kutovenko@gmail.com>
2023-02-24 15:42:58 +0300

    Add Contact entity

---
+++ src/main/java/com/clevertec/clevertectesttaskrest/domain/Contact.java
@@ -0,0 +1,38 @@
+package com.clevertec.clevertectesttaskrest.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@ToString
+@Table(name = "contact")
+public class Contact extends AbstractEntity {
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private Timestamp createdAt;
+}
