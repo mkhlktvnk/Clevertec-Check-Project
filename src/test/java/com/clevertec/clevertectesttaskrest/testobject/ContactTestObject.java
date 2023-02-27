2733e238a18022b7885a762d08e1c72cf22e057f
misha <mikhail.kutovenko@gmail.com>
2023-02-27 04:39:02 +0300

    Update test objects

--- src/test/java/com/clevertec/clevertectesttaskrest/testobject/ContactTestObject.java
+++ src/test/java/com/clevertec/clevertectesttaskrest/testobject/ContactTestObject.java
@@ -1,9 +1,13 @@
 package com.clevertec.clevertectesttaskrest.testobject;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ContactTestObject {
     private long id;
--- src/test/java/com/clevertec/clevertectesttaskrest/testobject/UserTestObject.java
+++ src/test/java/com/clevertec/clevertectesttaskrest/testobject/UserTestObject.java
@@ -1,11 +1,15 @@
 package com.clevertec.clevertectesttaskrest.testobject;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Collection;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class UserTestObject {
     private long id;
